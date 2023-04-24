c10b907f924c2600298a578d861b82619f6b9dcc
Tarsila <tarsila@alatazan.com>
2023-04-24 20:43:56 +0200

    Second Commit

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from views import views
+#this will import the variable "views" (right) from the "views-file"
 
 app = Flask(__name__)
 #initializes application
 
+app.register_blueprint(views, url_prefix="/views")
+
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
+#debug=True makes the website refresh on her own. So when we change things here, they're also changed in the website
