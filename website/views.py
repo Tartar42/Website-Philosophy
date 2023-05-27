c8edce30e933446d44ddef0e10817535878c0c15
Tarsila <tarsila@alatazan.com>
2023-05-27 08:46:33 +0200

    changes in base.html

--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
 #here we are storing all routes of website. ((Login page not, because it has to do with authentication), Home page, etc.)
 
-from flask import Blueprint, render_template, request, jsonify, redirect, url_for
+from flask import Blueprint, render_template
  #Blueprint has many roots / URLs inside it
 
 views = Blueprint("views", __name__)
@@ -8,6 +8,5 @@ views = Blueprint("views", __name__)
 @views.route("/")
 #this function will run, whenever we go to the /-route: ""
 def home():
-    return '<h1>Test</h1>'
-
+    return render_template("home.html")
 
